95d669910d46c82f8028360be5f38c237cd3e192
Jeff Holland <jeff.holland@gmail.com>
2012-03-08 17:20:37 -0600

    Adding SQL Server loop

--- display.py
+++ display.py
@@ -2,6 +2,7 @@ import curses
 import string
 from os import environ
 
+LINE_Y = 3          # Default Y location to start server listing
 
 class Window():
     def __init__(self):
@@ -9,11 +10,8 @@ class Window():
         self.initscr()
         self.default_refresh()
         key = self.screen.getch()
-        wintitle = curses.newwin(10, 20, 5, 5)
-        wintitle.border(0)
-        wintitle.addstr(1, 1, "TESTING NEW WINDOW", curses.color_pair(1))
-        wintitle.refresh()
         while key != 27:
+            self.check_servers()
             self.screen.addstr(5, 5, str(key), curses.color_pair(1))
             self.screen.refresh()
             key = self.screen.getch()
@@ -44,8 +42,29 @@ class Window():
                 curses.color_pair(2))
         self.screen.refresh()
 
+    def check_servers(self):
+        self.default_refresh()
+        line_y = LINE_Y
+        db_list = []
+        file = open("db.config")
+        for line in file:
+            if "FreeTDS" in line:
+                tds = line.strip().split('=')
+                freetds = tds[-1]
+            else:
+                db_data = line.strip().split(',')
+                db_list.append(db_data)
+        file.close()
+        return
+
     def define_pairs(self):
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+        #wintitle = curses.newwin(10, 20, 5, 5)
+        #wintitle.border(0)
+        #wintitle.addstr(1, 1, "TESTING NEW WINDOW", curses.color_pair(1))
+        #wintitle.refresh()
+
